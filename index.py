5d5d87ffbbdb5540240f105647137c663427f59c
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2019-12-07 11:04:01 -0600

    Proyecto terminado

---
+++ index.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, request, redirect, flash
+from methods import *
+
+file = ""
+
+app = Flask(__name__)
+
+# settings
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = 'mysecretkey'
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/uploadFile', methods=['POST'])
+def upload_file():
+    file = request.files['fileUpload']
+    global response
+    response = read_csv(file)
+    df = response.head()
+
+    return render_template("uploadFile.html", tables=[df.to_html(classes='data', header="true")], titles=df.columns.values, save_data = file)
+
+@app.route('/message', methods=['POST'])
+def message():
+
+    column = request.form['firstColumn']
+    graph = request.form['graph']
+    nameGraph = request.form['nameGraph']
+
+    if(graph != '0' and column != '0'):
+        graph_dataframe(response, column, graph, nameGraph)
+        flash("Graph Saved Successfully")
+
+        return redirect(url_for('index'))
+    else:
+        flash("Error trying save the graph")
+
+        return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    msg = "Error 404. Page not found"
+    return render_template('message.html', message=msg)
+
+@app.route('/cancel_message')
+def cancel_message():
+    flash('Operation canceled')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(port=4000, debug=True)
---
+++ methods.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_csv(routeFile):
+    route = routeFile
+    df = pd.read_csv(route)
+    return df
+
+def graph_dataframe(response, columnName, option, nameGraph):
+    df = response
+    df = df.dropna()
+
+    if(option == 'barra'):
+        plt.hist(df[columnName])
+    elif option == 'pastel':
+        plt.pie(df[columnName])
+    else:
+        plt.plot(df[columnName])
+    plt.savefig("{}.png".format(nameGraph))
+
+def show_csv(file):
+    df = read_csv(file)
+    return df.index
+
