97b3bac14c30129cc4f50999e0680b9b788d7951
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2019-12-10 10:49:10 -0600

    variables de session

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, session, escape
 from flask_caching import Cache
 from methods import read_csv, graph_dataframe
-
-file = ""
+import pandas as pd
 
 app = Flask(__name__)
+#SESSION_TYPE = 'filesystem'
 
 # settings
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = 'mysecretkey'
 
+#data = ''
+response = ''
+
 @app.route('/')
 def index():
+    session.clear()
     return render_template("home.html")
 
 @app.route('/uploadFile', methods=['POST'])
@@ -19,6 +23,8 @@ def upload_file():
     file = request.files['fileUpload']
     global response
     response = read_csv(file)
+    #global data
+    #data = request.args.get("data", response)
     df = response.head()
 
     return render_template("uploadFile.html", tables=[df.to_html(classes='data', header="true")], titles=df.columns.values)
@@ -29,12 +35,15 @@ def message():
     column = request.form['firstColumn']
     graph = request.form['graph']
     nameGraph = request.form['nameGraph']
-    info = response
+
+    #data_server = escape(data)
+    #df = pd.read_csv(data_server)
+    #print(df)
 
     if(graph != '0' and column != '0'):
-        print(info)
-        img_transform = graph_dataframe(info, column, graph, nameGraph)
-        print(img_transform)
+        #img_transform = graph_dataframe(data_server, column, graph, nameGraph)
+        img_transform = graph_dataframe(response, column, graph, nameGraph)
+        session.clear()
         flash("Graph Saved Successfully")
 
         return render_template("show_img.html", url_img = img_transform)
